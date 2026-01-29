7e1b8ffb783c8326344c226f19d24f6795269c61
Manoel Rodrigues <manoeldr03@gmail.com>
2026-01-28 21:00:33 -0300

    feat: banners principais

---
+++ scripts/banner.js
@@ -0,0 +1,16 @@
+const bannerSwiper = new Swiper('.banner-swiper', {
+    loop: true,
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+    },
+    effect: 'fade',
+    fadeEffect: {
+        crossFade: true
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    speed: 1000,
+});
