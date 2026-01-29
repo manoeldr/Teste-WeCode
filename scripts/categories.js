b0bfcac079d92928333aa0c6d1211b3661dc7b21
Manoel Rodrigues <manoeldr03@gmail.com>
2026-01-28 21:02:27 -0300

    feat: categorias

---
+++ scripts/categories.js
@@ -0,0 +1,6 @@
+const categoriesSwiper = new Swiper('.categories-swiper', {
+    slidesPerView: 'auto',
+    spaceBetween: 12,
+    freeMode: true,
+    grabCursor: true,
+});
--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,3 @@
 import './header.js';
 import './banner.js';
+import './categories.js';
